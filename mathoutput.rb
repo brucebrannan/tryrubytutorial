4f051100c1f405a93016c958f2020dfae698d137
Bruce Brannan <bruce.brannan@gmail.com>
2012-04-08 21:56:13 -0600

    adding files and removing .idea folder

--- mathoutput.rb
+++ mathoutput.rb
@@ -1,4 +1,4 @@
-#/usr/bin/env ruby
+#!/usr/bin/env ruby
 
 #Numeric and mathematic symbols
 puts 2+6
---
+++ stringoutput.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+#Say Your Name
+puts "Bruce"
+
+#Reverse Your Name
+puts "Bruce".reverse
+puts "Adventure Time".reverse
+
+#Counting the letters
+puts "Bruce".length
+
+#Repeat
+puts "Bruce " * 5
